73a6e6c0b2fcacfe8f4f572d7509dd9ae53692dd
epolepole <epolepole@gmail.com>
2018-09-28 21:12:06 +0200

    Add a different opening method

--- py_solutions/Algorithms/mine_sweeper.py
+++ py_solutions/Algorithms/mine_sweeper.py
@@ -1,3 +1,6 @@
+import queue
+
+
 def get_contiguous_directions():
     cont_set = set()
     for i in range(-1, 2):
@@ -5,6 +8,9 @@ def get_contiguous_directions():
             if i != 0 or j != 0:
                 cont_set.add((i, j))
     return cont_set
+
+
+dirs = get_contiguous_directions()
 
 
 def my_add(a, b):
@@ -37,8 +43,35 @@ def mine_sweeper(mines, h, w):
                 if board[i][j] != -1:
                     board[i][j] += 1
 
-    print(board)
+    # print(board)
     return board
+
+
+def is_bounded(i, j, h, w):
+    return 0 <= i < h and 0 <= j < w
+
+
+# Implement your function below.
+def click_a(field, h, w, i, j):
+    if is_bounded(i, j, h, w) and field[i][j] == 0:
+        field[i][j] = -2
+        for x, y in dirs:
+            click_a(field, h, w, i + x, j + y)
+    return field
+
+
+def click(field, h, w, i, j):
+    to_check = queue.Queue()
+    to_check.put((i, j))
+    while not to_check.empty():
+        a, b = to_check.get()
+        if field[a][b] == 0:
+            field[a][b] = -2
+            for da, db in dirs:
+                i, j = a + da, b + db
+                if is_bounded(i, j, h, w):
+                    to_check.put((i, j))
+    return field
 
 
 def test_mine_sweeper():
@@ -58,3 +91,33 @@ def test_mine_sweeper():
         [-1, -1, 1, 0],
         [3, 4, 3, 1],
         [1, -1, -1, 1]]
+
+    field1 = [[0, 0, 0, 0, 0],
+              [0, 1, 1, 1, 0],
+              [0, 1, -1, 1, 0]]
+
+    field1_opened = [[-2, -2, -2, -2, -2],
+                     [-2, 1, 1, 1, -2],
+                     [-2, 1, -1, 1, -2]]
+
+    assert mine_sweeper([[2, 2]], 3, 5) == field1
+    assert click(field1, 3, 5, 2, 2) == field1
+    assert click(field1, 3, 5, 1, 4) == field1_opened
+    assert click_a(field1, 3, 5, 2, 2) == field1
+    assert click_a(field1, 3, 5, 1, 4) == field1_opened
+
+    field2 = [[-1, 1, 0, 0],
+              [1, 1, 0, 0],
+              [0, 0, 1, 1],
+              [0, 0, 1, -1]]
+
+    field2_opened = [[-1, 1, -2, -2],
+                     [1, 1, -2, -2],
+                     [-2, -2, 1, 1],
+                     [-2, -2, 1, -1]]
+
+    assert mine_sweeper([[0, 0], [3, 3]], 4, 4) == field2
+    assert click(field2, 4, 4, 0, 1) == field2
+    assert click(field2, 4, 4, 1, 3) == field2_opened
+    assert click_a(field2, 4, 4, 0, 1) == field2
+    assert click_a(field2, 4, 4, 1, 3) == field2_opened
